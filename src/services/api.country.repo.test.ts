abb26bb74a27d46b759c9ed117214d891eda855f
mariano quiroga <mariano.eq@gmail.com>
2023-02-14 15:24:13 +0100

    Add loadCountries test

--- src/services/api.country.repo.test.ts
+++ src/services/api.country.repo.test.ts
@@ -1,14 +1,17 @@
 import { ApiCountryRepo } from "./api.country.repo";
 
 describe("Given a class to build api methods", () => {
-  describe("When the clas is instaciate", () => {
+  describe("When the class is instanciate", () => {
     let repo: ApiCountryRepo;
     beforeEach(() => {
       repo = new ApiCountryRepo();
     });
 
-    test("Then should called its methods", () => {
+    test("Then should an instance of ApiCoutryRepo", () => {
       expect(repo).toBeInstanceOf(ApiCountryRepo);
+    });
+    test("Then loadCountry method should be a function", () => {
+      expect(typeof repo.loadCountries).toBe("function");
     });
   });
 });
