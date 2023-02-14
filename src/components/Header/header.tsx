14b235849d66362711b4d3d4be4b039be4714302
CarlosRuiz <carua97@gmail.com>
2023-02-14 18:07:21 +0100

    Header css and logo added

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,8 +1,11 @@
+import "./header.css";
+
 export function Header() {
+  const logo = "./images/logo-wikicountry.png";
   return (
-    <header>
-      <img src="" alt="Logo wikicountry" />
-      <h1>WikiCountry</h1>
+    <header className="header">
+      <img className="header__image" src={logo} alt="Logo wikicountry" />
+      <h1 className="header__title">WikiCountry</h1>
     </header>
   );
 }
