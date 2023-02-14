20732c29ad270aeda4c7dde7e25d93b046316083
CarlosRuiz <carua97@gmail.com>
2023-02-14 20:12:23 +0100

    Add details folder, tsx, css and test

---
+++ src/components/Details/details.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { Details } from "./details";
+
+describe("Given Header component", () => {
+  describe("When it is render", () => {
+    test("Then it should have the title in the screen", () => {
+      render(
+        <>
+          <Details></Details>
+        </>
+      );
+      const element = screen.getByText(/Capital/i);
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/components/Details/details.tsx
@@ -0,0 +1,44 @@
+import "./details.css";
+
+export function Details() {
+  //const logo = "./images/logo-wikicountry.png";
+  return (
+    <section className="details">
+      <div className="details-top">
+        <button>Home</button>
+        <button>Favourite</button>
+      </div>
+      <ul className="details-list">
+        <li className="details-list__item details-list__item--center">
+          Nombre pais
+        </li>
+        <li className="details-list__item details-list__item--center">
+          Bandera
+        </li>
+        <li className="details-list__item">
+          <p>Capital</p>
+          <p>Algo</p>
+        </li>
+        <li className="details-list__item">
+          <p>Lenguaje</p>
+          <p>Algo</p>
+        </li>
+        <li className="details-list__item">
+          <p>Moneda</p>
+          <p>Algo</p>
+        </li>
+        <li className="details-list__item">
+          <p>Población</p>
+          <p>Algo</p>
+        </li>
+        <li className="details-list__item">
+          <p>Continente</p>
+          <p>Algo</p>
+        </li>
+        <li className="details-list__item details-list__item--center">
+          Escudo de armas
+        </li>
+      </ul>
+    </section>
+  );
+}
