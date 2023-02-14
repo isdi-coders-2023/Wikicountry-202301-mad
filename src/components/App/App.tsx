0da99833c5ed6fd8912327a167b01138692a1700
CarlosRuiz <carua97@gmail.com>
2023-02-14 20:20:57 +0100

    Resolve merge problems netlify deploy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Details } from "../Details/details";
 import { Header } from "../Header/header";
 import "./App.css";
-import { ApiCountryRepo } from "./services/api.country.repo";
 
 function App() {
   return (
