435eb37179d4cce3aaed7e515e460498a33c5902
rocioRichi <paragit@gmail.com>
2023-02-14 14:05:01 +0100

    Add actions & reducer

---
+++ src/reduce/components/reducer/countries.action.creator.ts
@@ -0,0 +1,20 @@
+import { ProtoCountryStrucuture } from "../../../model/country";
+import { countriesActions } from "./countries.actions";
+
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+export interface CountryActions extends Action {
+  payload: ProtoCountryStrucuture | ProtoCountryStrucuture[];
+}
+
+export const loadCountryCreator = (
+  payload: ProtoCountryStrucuture[]
+): CountryActions => {
+  return {
+    type: countriesActions.load,
+    payload,
+  };
+};
