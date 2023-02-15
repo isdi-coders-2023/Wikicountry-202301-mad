ec432fe5dc6dac142974c5900d89b8f80bc4649d
mariano quiroga <mariano.eq@gmail.com>
2023-02-15 09:46:12 +0100

    Create api repo & delete unnecessary code in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,27 @@
+<<<<<<< HEAD:src/App.tsx
+import React, { useEffect, useMemo } from "react";
+=======
 import React from "react";
 import { Header } from "../Header/header";
+>>>>>>> main:src/components/App/App.tsx
 import "./App.css";
+import { ApiCountryRepo } from "./services/api.country.repo";
 
 function App() {
+<<<<<<< HEAD:src/App.tsx
+  const load = useMemo(() => new ApiCountryRepo(), []);
+
+  useEffect(() => {
+    load.loadCountries();
+  }, [load]);
+  return (
+    <div className="App">
+      <h1>Working in progress</h1>
+    </div>
+  );
+=======
   return <Header></Header>;
+>>>>>>> main:src/components/App/App.tsx
 }
 
 export default App;
