a6344ae8b04a4a05d3e0833dd17fc833c01ebade
mariano quiroga <mariano.eq@gmail.com>
2023-02-15 09:51:35 +0100

    api repo tests

---
+++ src/services/api.country.repo.test.ts
@@ -0,0 +1,41 @@
+import { ApiCountryRepo } from "./api.country.repo";
+
+describe("Given a class to build api methods", () => {
+  describe("When the class is instaciated", () => {
+    let repo: ApiCountryRepo;
+    beforeEach(() => {
+      repo = new ApiCountryRepo();
+    });
+
+    test("Then loadCountries method should return an array", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue([]),
+      });
+      const result = await repo.loadCountries();
+      expect(result).toEqual([]);
+    });
+    test("Then getCountryByName should return a country", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue({
+          common: "",
+          official: "",
+        }),
+      });
+      const r = await repo.getCountryByName({
+        common: "",
+        official: "",
+      });
+      expect(r).toEqual({
+        common: "",
+        official: "",
+      });
+    });
+    test("Then getCountryByRegion should return a country", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue("america"),
+      });
+      const r = await repo.getCountryByRegion("america");
+      expect(r).toBe("america");
+    });
+  });
+});
