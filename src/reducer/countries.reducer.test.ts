3575923db80e33cd7ebc5b4a2ad9a37a1ff73528
rocioRichi <paragit@gmail.com>
2023-02-15 12:15:15 +0100

    Remove  mistakes, but doesnt work

--- src/reducer/countries.reducer.test.ts
+++ src/reducer/countries.reducer.test.ts
@@ -1,21 +1,22 @@
 import { CountryActions } from "./countries.action.creator";
+import { countriesActions } from "./countries.actions";
 
-const state:string = "hola";
+const state: string = "hola";
 
-describe("country reducer", () =>
-  it("Function countryReducer", () => {
-    function countriesReducer(
-      state: string = 'hola',
-      action: CountryActions
-    ): string {
-      switch (action.type) {
-        case countriesActions.load:
-          return state;
-        default:
-          return state;
-      }
+describe("country reducer", () => {
+  function countriesReducer(state: string, action: CountryActions): string {
+    switch (action.type) {
+      case countriesActions.load:
+        return state;
+      default:
+        return state;
     }
-    test("Should return state=hola", () => {
-      expect(console.log(`${state}= hola`));
-    });
   }
+  it("should equal 4", () => {
+    expect(countriesReducer("hola", 2)).toBe("hola");
+  });
+
+  test("also should equal 4", () => {
+    expect(countriesReducer("hola", 3)).toBe("hola");
+  });
+});
