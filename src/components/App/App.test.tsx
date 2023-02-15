f0d25ddff7f886a4959149c83f1af233d49f2f68
CarlosRuiz <carua97@gmail.com>
2023-02-15 17:49:40 +0100

    App modified for testing

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -8,12 +8,9 @@ jest.mock("../App-router/app-router");
 
 describe("Given App component", () => {
   describe("When it is render", () => {
-    test("Then it should call Header", () => {
+    test("Then it should call Header and AppRouter", () => {
       render(<App />);
       expect(Header).toHaveBeenCalled();
-    });
-    test("Then it should call AppRouter", () => {
-      render(<App />);
       expect(AppRouter).toHaveBeenCalled();
     });
   });
