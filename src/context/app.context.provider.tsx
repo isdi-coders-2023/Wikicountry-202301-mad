2740a699a2569739d9a6b99bc25fff939f229a9f
mariano quiroga <mariano.eq@gmail.com>
2023-02-15 13:03:55 +0100

    Add api repo and call to api repo into contProv

--- src/context/app.context.provider.tsx
+++ src/context/app.context.provider.tsx
@@ -1,9 +1,12 @@
+import { useMemo } from "react";
+import { useWorld } from "../hook/use.world";
+import { ApiCountryRepo } from "../services/api.country.repo";
 import { worldContext } from "./app.context";
 export function WorldContextProvider({ children }: { children: JSX.Element }) {
-  const worldRepo = "";
-  console.log(worldRepo);
+  const worldRepo = useMemo(() => new ApiCountryRepo(), []);
+  const context = useWorld(worldRepo);
 
   return (
-    <worldContext.Provider value={"context"}>{children}</worldContext.Provider>
+    <worldContext.Provider value={context}>{children}</worldContext.Provider>
   );
 }
