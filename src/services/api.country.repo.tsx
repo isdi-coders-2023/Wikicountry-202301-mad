e4294af21a0084bd9074b70c128be81628e4f32f
mariano quiroga <mariano.eq@gmail.com>
2023-02-15 16:17:33 +0100

    Add type for name property

--- src/services/api.country.repo.tsx
+++ src/services/api.country.repo.tsx
@@ -10,6 +10,10 @@ export interface CountryApiRepoStructure {
   ): Promise<ProtoCountryStrucuture>;
 }
 
+export type ResponseName = {
+  name: string;
+};
+
 export class ApiCountryRepo {
   url: string;
   constructor() {
@@ -21,9 +25,7 @@ export class ApiCountryRepo {
     return data;
   }
 
-  async getCountryByName(
-    name: ProtoCountryStrucuture["name"]
-  ): Promise<ProtoCountryStrucuture> {
+  async getCountryByName(name: ResponseName): Promise<ProtoCountryStrucuture> {
     const response = await fetch(this.url + "/name/" + name);
     const data = await response.json();
     return data;
