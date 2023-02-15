f7747e2d38fb7da889c5800a6841e11a8d75537f
mariano quiroga <mariano.eq@gmail.com>
2023-02-15 16:19:40 +0100

    create custom hook with api public functions

--- src/hook/use.world.tsx
+++ src/hook/use.world.tsx
@@ -1,7 +1,49 @@
-import { ApiCountryRepo } from "../services/api.country.repo";
-
+import { ApiCountryRepo, ResponseName } from "../services/api.country.repo";
+import { useCallback, useState } from "react";
+import { ProtoCountryStrucuture } from "../model/country";
 export type UseWorldStructure = ReturnType<typeof useWorld>;
 export function useWorld(repo: ApiCountryRepo) {
-  const greet = "hola";
-  return greet;
+  const initialState: ProtoCountryStrucuture[] = [];
+
+  const [countries, setCountries] = useState(initialState);
+
+  const handleError = (error: Error) => {
+    console.log(error.message);
+  };
+
+  const loadCountries = useCallback(async () => {
+    try {
+      const countries = await repo.loadCountries();
+      setCountries(countries);
+    } catch (error) {
+      handleError(error as Error);
+    }
+  }, [repo]);
+
+  const getCountryByName = async (name: ResponseName) => {
+    try {
+      const countryByName = await repo.getCountryByName(name);
+      setCountries([...countries, countryByName]);
+    } catch (error) {
+      handleError(error as Error);
+    }
+  };
+
+  const getCountryByRegion = async (
+    region: ProtoCountryStrucuture["region"]
+  ) => {
+    try {
+      const countryByRegion = await repo.getCountryByRegion(region);
+      setCountries([...countries, countryByRegion]);
+    } catch (error) {
+      handleError(error as Error);
+    }
+  };
+
+  return {
+    countries,
+    loadCountries,
+    getCountryByName,
+    getCountryByRegion,
+  };
 }
