43dd2884dee5d5d82d6246aa24b3b0951c98127f
CarlosRuiz <carua97@gmail.com>
2023-02-16 14:19:41 +0100

    Modify  card component

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -8,9 +8,7 @@ export function Card({ country }: CountryProps) {
   return (
     <li className="country-card">
       <img src={country.flags.png} alt={country.flags.alt} />
-      <div className="country-card__name" role="country-name">
-        {country.name.common}
-      </div>
+      <div className="country-card__name">{country.name.common}</div>
     </li>
   );
 }
