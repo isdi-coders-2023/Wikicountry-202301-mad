abdbf17e337efe4f8bf07a4a22433e2172c55659
rocioRichi <paragit@gmail.com>
2023-02-17 17:32:26 +0100

    Add home & fav icon

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -4,9 +4,12 @@ type HeaderProps = { children: JSX.Element };
 
 export function Header({ children }: HeaderProps) {
   const logo = "./images/logo-wikicountry.png";
+  const fav = "./images/img-fav.png";
+
   return (
     <header className="header">
       <img className="header__image" src={logo} alt="Logo wikicountry" />
+
       <h1 className="header__title">WikiCountry</h1>
       {children}
     </header>
