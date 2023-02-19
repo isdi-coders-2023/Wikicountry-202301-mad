84444a53eadff776101418090f6e903c757727dd
Carlos <carua97@gmail.com>
2023-02-19 19:40:48 +0100

    Set pager buttons for cards

--- src/components/Cards/cards.tsx
+++ src/components/Cards/cards.tsx
@@ -1,11 +1,22 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import "./cards.css";
 import { Card } from "../Card/card";
 import { WorldContext } from "../../context/app.context";
-import { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ProtoCountryStrucuture } from "../model/country";
 
 export function Cards() {
+  const buttonPrev = "./images/button-prev";
+  const buttonNext = "./images/button-next";
+
   const { stateCountries, loadCountries } = useContext(WorldContext);
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(10);
+
+  const hadlerPrevNextButton = (num: number) => {
+    setSecondNum(secondNum + num);
+    setFirstNum(firstNum + num);
+  };
 
   useEffect(() => {
     loadCountries();
@@ -14,10 +25,30 @@ export function Cards() {
   return (
     <section className="cards">
       <ul className="cards__ul">
-        {stateCountries?.countries.map((item: ProtoCountryStrucuture) => (
-          <Card key={item.population} country={item}></Card>
-        ))}
+        {stateCountries.countries
+          .slice(firstNum, secondNum)
+          .map((item: ProtoCountryStrucuture) => (
+            <Card key={item.population} country={item}></Card>
+          ))}
       </ul>
+      <div>
+        <button
+          disabled={firstNum === 0 ? true : false}
+          onClick={() => {
+            hadlerPrevNextButton(-10);
+          }}
+        >
+          <img src={buttonPrev} alt="Previous page" />
+        </button>
+        <button
+          disabled={secondNum === 250 ? true : false}
+          onClick={() => {
+            hadlerPrevNextButton(10);
+          }}
+        >
+          <img src={buttonNext} alt="Next Page" />
+        </button>
+      </div>
     </section>
   );
 }
